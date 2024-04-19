faa4ac04ea058387c6df2ad5f40f2f53ffd3773f
Dj3y <dkyamilova@gmail.com>
2024-04-19 16:32:50 +0200

    Ajout premiere version

---
+++ assets/js/script.js
@@ -0,0 +1,159 @@
+const collection = [
+    {
+       nom: "Last Vegas",
+       anneeProduction: 2013,
+       realisateur: "John Turteltaub",
+       genre: ["Comedie"],
+       acteurs: ["Michael Douglas", " Robert De Niro", " Morgan Freeman", " Kevin Kline", " Mary Steenbugen"], 
+       resume: "Billy, Paddy, Archie et Sam sont amis depuis 60 ans. Alors qu'il est sur le point de se marier avec une femme qui a moins de la moitié de son âge, Billy invite ses trois comparses à Las Vegas pour un enterrement de vie de garçon. Archie est heureux d'avoir enfin de la liberté et Sam ne pense qu'à s'envoyer en l'air. Il n'y a que Paddy qui hésite, de peur d'envenimer la situation qui est déjà tendue avec Billy. Le quatuor décide malgré tout de se réunir et de faire les 400 coups au casino et dans les discothèques.",
+       photo: "../assets/images/LastVegas.jpg",
+       bandeAnnonce: "https://www.youtube.com/embed/TvK3m0wJutI?si=crnLBUxC54tMRDiy",
+    },
+    {
+        nom: "Taxi",
+        anneeProduction: 1998,
+        realisateur: "Gérard Pirès",
+        genre: ["Comédie", " Action"],
+        acteurs: ["Samy Naceri", " Frédéric Diefenthal", " Marion Cotillard", " Bernard Farcy"], 
+        resume:"Daniel est un fou du volant. Cet ex-livreur de pizzas est aujourd'hui chauffeur de taxi et sait échapper aux radars les plus perfectionnés. Pourtant, un jour, il croise la route d'Emilien, policier recalé pour la huitième fois à son permis de conduire. Pour conserver son taxi, il accepte le marché que lui propose Emilien : l'aider à démanteler un gang de braqueurs de banques qui écume les succursales de la ville à bord de puissants véhicules. Road-movie urbain sur un scénario de Luc Besson, également producteur.",
+        photo: "../assets/images/TaxiPoster.jpg",
+        bandeAnnonce: "https://www.youtube.com/embed/OmCmMkGN7bQ?si=YLUB1sNwb_SihmER",
+    },
+    {
+        nom: "Banlieue 13",
+        anneeProduction: 2004,
+        realisateur: "Pierre Morel",
+        genre: ["Action", " Aventure"],
+        acteurs: ["David Belle", " Cyril Raffaelli", " Larbi Naceri", " Dany Verissimo-Petit", " Élodie Yung"], 
+        resume: "Paris, 2013. Damien est l'élite de la police. Officier d'une unité spéciale d'intervention, expert en arts martiaux, il est passé maître dans l'art de l'infiltration et sait mener à terme ses opérations par des actions rapides, précises et néanmoins musclées.Et c'est bien la mission la plus extrême de sa carrière qui vient de lui être confiée : une arme de destruction massive a été dérobée par le plus puissant gang de la banlieue. Damien est chargé d'infiltrer dans le secteur pour désamorcer la bombe ou la récupérer.",
+        photo: "../assets/images/banlieue13.jpg",
+        bandeAnnonce: "https://www.youtube.com/embed/BJPy7e5hvSg?si=aJSzzAW3_3UuqrMr",
+    },
+    {
+        nom: "Safe",
+        anneeProduction: 2012,
+        realisateur: "Boaz Yakin",
+        genre: ["Crime", " Thriller", " Action"],
+        acteurs: ["Jason Statham", " Catherine Chan", " Chris Sarandon", " James Hong"],  
+        resume: "Un ancien agent secret sauve une petite fille à la mémoire exceptionnelle, déclenchant ainsi une guerre des gangs entre les Triades, la mafia russe et des politiciens new-yorkais corrompus. Tous veulent mettre la main sur l'enfant, la seule à détenir la combinaison d'un coffre-fort très convoité.",
+        photo: "../assets/images/safe.jpg",
+        bandeAnnonce: "https://www.youtube.com/embed/tnEz_-i2SJc?si=DfuzXhvWfcgVVHlO",
+    },
+    {
+        nom: "Fast & Furious 7",
+        anneeProduction: 2015,
+        realisateur: "James Wan",
+        genre: ["Crime", " Thriller", " Action"],
+        acteurs: ["Vin Diesel", " Paul Walker", " Jason Statham", " Michelle Rodriguez", " Jordana Brewster", " Dwayne Johnson"], 
+        resume: "Laissé à moitié mort par Dominic Toretto et son clan de voleur de voitures as du volant, le criminel Owen Shaw gît sur un lit d'hôpital à Londres. Déterminé à le venger, son frère Deckard Shaw commet des attaques vicieuses contre les membres de la famille de Toretto, notamment contre sa sœur, son beau-frère et leur petit garçon. Une aide inespérée leur parvient en la personne de Frank Petty, agent des services secrets américains aux ressources impressionnantes. Il promet assistance à Dominic, en échange de quoi ce dernier devra libérer une talentueuse informaticienne, nommée Ramsey, qui a mis au point un logiciel de surveillance ultraperfectionné baptisé « God's Eye ». La jeune femme est en effet retenue prisonnière par Jakande, un dangereux terroriste, au milieu des inaccessibles montagnes de l'Azerbaïdjan. Le clan de Dominic se reforme donc pour cette dangereuse mission.",
+        photo: "../assets/images/fastFurious7.jpg",
+        bandeAnnonce: "https://www.youtube.com/embed/3Wu2BhFuc4c?si=MgI1nJQLuZzRbQ25",
+    },
+    {
+        nom: "Jumanji : bienvenue dans la jungle",
+        anneeProduction: 2017,
+        realisateur: "Jake Kasdan",
+        genre: ["Fantastique", " Action", " Aventure", " Comédie"],
+        acteurs: ["Dwayne Johnson", " Kevin Hart", " Jack Black", " Karen Gillan"], 
+        resume: "Quatre lycéens découvrent une vieille console contenant un jeu vidéo dont ils n'avaient jamais entendu parler : Jumanji. En voulant jouer, ils se retrouvent mystérieusement propulsés dans la jungle de Jumanji, où ils deviennent leurs avatars. Spencer est désormais un aventurier musclé, Fridge, le footeux, devient un véritable Einstein, Bethany, la fille si populaire au lycée, se transforme en professeur d'âge mûr et la timide Martha est maintenant une redoutable combattante. Ils vont rapidement découvrir qu'il ne suffit pas de jouer à Jumanji, il faut y survivre… Pour vaincre le jeu et revenir dans le monde réel, ils vont devoir affronter les pires dangers, découvrir ce qu'Alan Parrish a laissé vingt ans auparavant, et changer leur façon de voir la vie et eux-mêmes. Sinon, ils resteront prisonniers de Jumanji pour toujours…",
+        photo: "../assets/images/jumanji.jpg",
+        bandeAnnonce: "https://www.youtube.com/embed/oZI2njF5rx0?si=um5vScyAoQAa9Yne",
+    },
+    {
+        nom: "Antartica, prisonniers du froid",
+        anneeProduction: 2006,
+        realisateur: "Frank Marshall",
+        genre: ["Action", " Aventure", " Drame"],
+        acteurs: ["Paul Walker", " Bruce Greenwood", " Moon Bloodgood", " Jason Biggs"],  
+        resume: "Alors qu'ils mènent une expédition scientifique en plein Antarctique, le guide Jerry Shepard et le géologue Davis McClaren sont victimes d'un accident qui aurait pu être fatal si leurs chiens de traîneau ne les avaient pas tirés d'affaire. Évacués d'urgence dans des conditions extrêmes, Jerry est contraint d'abandonner ses chiens au cœur d'un des hivers les plus redoutables que la zone polaire ait connus. Livrés à eux-mêmes, les huit animaux vont tenter de survivre dans une nature aussi majestueuse qu'hostile. Incapable d'oublier ses compagnons à quatre pattes, Jerry tente par tous les moyens de retourner sur place, mais les conditions météo l'en empêchent. Face aux pires conditions qui soient, humains et chiens vont tout affronter pour se retrouver...",
+        photo: "../assets/images/antartica.jpg",
+        bandeAnnonce: "https://www.youtube.com/embed/9lZ14xipTYY?si=-VP8dWN-nsS02aGl",
+    },
+    {
+        nom: "Annabelle",
+        anneeProduction: 2014,
+        realisateur: "John R. Leonetti",
+        genre: ["Horreur", " Mystère", " Thriller"],
+        acteurs: ["Annabelle Wallis", " Ward Horton", " Tony Amendola", " Alfre Woodard"], 
+        resume: "John Form est certain d'avoir déniché le cadeau de ses rêves pour sa femme Mia, qui attend un enfant. Il s'agit d'une poupée ancienne, très rare, habillée dans une robe de mariée d'un blanc immaculé. Mais Mia, d'abord ravie par son cadeau, va vite déchanter. Une nuit, les membres d'une secte satanique s'introduisent dans leur maison et agressent sauvagement le couple, paniqué. Et ils ne se contentent pas de faire couler le sang et de semer la terreur - ils donnent vie à une créature monstrueuse, pire encore que leurs sinistres méfaits, permettant aux âmes damnées de revenir sur Terre : Annabelle…",
+        photo: "../assets/images/annabelle.webp",
+        bandeAnnonce: "https://www.youtube.com/embed/LUCOp6efZmg?si=L1M5JZC1fcyXT70L",
+    },
+    {
+        nom: "Ça",
+        anneeProduction: 2017,
+        realisateur: "Andy Muschietti",
+        genre: ["Horreur", " Fantastique"],
+        acteurs: ["Jaeden Martell", " Sophia Lillis", " Jack Dylan Grazer", " Finn Wolfhard", " Jeremy Ray Taylor", " Wyatt Oleff", " Bill Skarsgård"],  
+        resume: "À Derry, dans le Maine, aux États-Unis, 7 gamins, ayant du mal à s'intégrer, se sont regroupés au sein du Club des Ratés. Rejetés par leurs camarades, ils sont les cibles favorites des gros durs de l'école. Ils ont aussi en commun 'avoir éprouvé leur plus grande terreur face à un terrible prédateur métamorphe qu'ils appellent Ça...",
+        photo: "../assets/images/ça.webp",
+        bandeAnnonce: "https://www.youtube.com/embed/Tw3yT-qAbvc?si=L1Jx-5aGrZCvMaO8",
+    },
+    {
+        nom: "La nonne",
+        anneeProduction: 2018,
+        realisateur: "Corin Hardy",
+        genre: ["Horreur", " Mystère", " Thriller"],
+        acteurs: ["Taissa Farmiga", " Demián Bichir", " Bonnie Aarons", " Jonas Bloquet"], 
+        resume: "Quand on apprend le suicide d'une jeune nonne dans une abbaye roumaine, la stupéfaction est totale dans l'Église catholique. Le Vatican missionne aussitôt un prêtre au passé trouble et une novice pour mener l'enquête. Risquant leur vie, les deux ecclésiastiques doivent affronter une force maléfique - la nonne démoniaque de « Conjuring 2 » - qui bouscule leur foi et menace de détruire leur âme.",
+        photo: "../assets/images/laNonne.jpeg",
+        bandeAnnonce: "https://www.youtube.com/embed/hxRjOBRINu0?si=9L6XSvS7XVHZEcXS",
+    },
+];
+
+// console.log(collection[8]["nom"]);
+function createCard(){
+    
+    // Creation de la division qui va contenir la card
+    const section = document.querySelector("section");
+    const h1 = document.createElement("h1");
+    section.appendChild(h1);
+    h1.innerHTML = "Ma collection des films";
+    const pInfo =document.createElement("p");
+    section.appendChild(pInfo);
+    pInfo.innerHTML = "Vous trouverez sur cette page une partie de ma collection des films. Cette collection va continuer à être enrichie avec le temps.";
+    const div = document.createElement("div");
+    div.id ="container";
+    section.appendChild(div);
+    for(const item of collection){
+        const divCard = document.createElement("div");
+        div.appendChild(divCard);
+        // Creation de balise pour l'image
+        const image = document.createElement("img");
+        divCard.appendChild(image);
+        let srcImg = item.photo;
+        image.setAttribute("src", srcImg);
+        // Creation de balise pour le nom du film
+        const nom = document.createElement("h2");
+        divCard.appendChild(nom); 
+        nom.innerHTML = item.nom;  
+        // Creation de balise pour le realisateur
+        const realisateur = document.createElement("h3");
+        divCard.appendChild(realisateur); 
+        realisateur.id = "realisateur";
+        realisateur.innerHTML = item.realisateur; 
+        // Creation de balise pour le genre
+        const genre = document.createElement("h4");
+        divCard.appendChild(genre);
+        genre.innerHTML = "Genre: " + item.genre;
+        // Creation de balise pour les acteurs
+        const acteur = document.createElement("h3");
+        divCard.appendChild(acteur);
+        acteur.id = "acteur";
+        acteur.innerHTML = "Casting: " + item.acteurs;
+        // Creation de balise pour l'année de production
+        const anneeProd = document.createElement("h5");
+        divCard.appendChild(anneeProd);
+        anneeProd.innerHTML = "L'année de production: " + item.anneeProduction;
+        // Creation de balise pour le résumé
+        const resume = document.createElement("p");
+        divCard.appendChild(resume);
+        resume.innerHTML = item.resume;
+        const bandeAnnonce = document.createElement("iframe");
+        divCard.appendChild(bandeAnnonce);
+        let srcVideo = item.bandeAnnonce;
+        bandeAnnonce.setAttribute("src", srcVideo);
+        bandeAnnonce.innerHTML = item.bandeAnnonce;
+    }
+}
+createCard();
